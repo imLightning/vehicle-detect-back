c4803dfc0ee8aaf86a53fbb7c6c837b787dd09fe
imLightning <2744002576@qq.com>
2024-06-06 15:25:28 +0800

    新增上传录像功能

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import cv2
+from flask import Flask, request
 from flask_cors import cross_origin
 
 app = Flask(__name__, static_folder='static')
@@ -7,6 +8,16 @@ app = Flask(__name__, static_folder='static')
 @cross_origin(origins='http://localhost:8080')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+@app.route('/recordUpload', methods=['POST'])
+@cross_origin(origins='http://localhost:8080')
+def record_upload():  # put application's code here
+    record = request.files['record']
+    # 文件写入磁盘
+    record.save('./tests/' + record.filename)
+    return 'success'
+
+
 
 if __name__ == '__main__':
     app.run()
